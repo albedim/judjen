a80e81ac3ce013a1f4fefeb8f2c6e24887e76635
Alberto Di Maio <albertodimaio05@gmail.com>
2024-03-10 01:12:20 +0100

    [improvement] - homepage

--- src/components/notification/panel/index.tsx
+++ src/components/notification/panel/index.tsx
@@ -28,43 +28,45 @@ const NavBarNotificationPanel: React.FC<NavBarNotificationPanelProps> = ( props 
   }
 
   return(
-    <div className="pr-0 z-50 absolute mt-16 right-4 bg-[white] rounded-md border p-4">
-      <div className="mr-4 justify-between flex pb-2 border-b">
-        <p className="font-semibold text-lg font-cabin">Notifiche</p>
-        <button disabled={props.notifications.length == 0} onClick={() => markAsRead()}>
-          <p className="text-[#668AE4] font-semibold text-sm font-cabin">Segna come lette</p>
-        </button>
-      </div>
-      <div style={{ width: 340, maxHeight: 540 }} className="overflow-y-scroll mt-4">
-        {props.notifications.length > 0 ? (
-          props.notifications.map((notification: Notification) => (
-            <div className="justify-between pt-1 items-center flex pl-0 p-4">
-              <div className="flex">
-                <button onClick={() => navigate("/user/" + notification.target.user_id)}>
-                  <Avatar
-                    size={42}
-                    name={notification.target.anonymous_name}
-                    variant="beam"
-                    colors={["#668AE4", "#4e6dba", "#3a57a1", "#526db3", "#6583cf"]}
-                  />
-                </button>
-                <div className="pl-4">
-                  <button onClick={() => navigate(notification.href)}><p style={{ textAlign: 'left', maxWidth: 194 }} >{notification.content}</p></button>
-                  <p className="text-[gray] font-cabin text-sm">{formatDate(notification.datetime)}</p>
+    <div className="z-20 absolute w-screen h-screen">
+      <div className="pr-0 fixed mt-20 right-4 bg-[white] rounded-md border p-4">
+        <div className="mr-4 justify-between flex pb-2 border-b">
+          <p className="font-semibold text-lg font-cabin">Notifiche</p>
+          <button disabled={props.notifications.length == 0} onClick={() => markAsRead()}>
+            <p className="text-[#668AE4] font-semibold text-sm font-cabin">Segna come lette</p>
+          </button>
+        </div>
+        <div style={{ width: 340, maxHeight: 540 }} className="overflow-y-scroll mt-4">
+          {props.notifications.length > 0 ? (
+            props.notifications.map((notification: Notification) => (
+              <div className="justify-between pt-1 items-center flex pl-0 p-4">
+                <div className="flex">
+                  <button onClick={() => navigate("/user/" + notification.target.user_id)}>
+                    <Avatar
+                      size={42}
+                      name={notification.target.anonymous_name}
+                      variant="beam"
+                      colors={["#668AE4", "#4e6dba", "#3a57a1", "#526db3", "#6583cf"]}
+                    />
+                  </button>
+                  <div className="pl-4">
+                    <button onClick={() => navigate(notification.href)}><p style={{ textAlign: 'left', maxWidth: 194 }} >{notification.content}</p></button>
+                    <p className="text-[gray] font-cabin text-sm">{formatDate(notification.datetime)}</p>
+                  </div>
+                </div>
+                <div className="items-center justify-around flex">
+                  <a title="Segna come letta">
+                    <button onClick={() => markAsRead(notification.notification_id)}>
+                      <FaCircle className="transition-all hover:opacity-80" size={10.4} color="#668AE4"/>
+                    </button>
+                  </a>
                 </div>
               </div>
-              <div className="items-center justify-around flex">
-                <a title="Segna come letta">
-                  <button onClick={() => markAsRead(notification.notification_id)}>
-                    <FaCircle className="transition-all hover:opacity-80" size={10.4} color="#668AE4"/>
-                  </button>
-                </a>
-              </div>
-            </div>
-          ))
-        ):(
-          <NotFound message="Non sono state trovate notifiche"/>
-        )}
+            ))
+          ):(
+            <NotFound message="Non sono state trovate notifiche"/>
+          )}
+        </div>
       </div>
     </div>
   )
